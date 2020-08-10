6be921329ac98a9637e971b03823b6246cc71f79
Nina Zakharenko <nzakharenko@gmail.com>
2020-08-09 17:20:43 -0700

    Add a language label to code blocks

--- static/js/learn.js
+++ static/js/learn.js
@@ -1,3 +1,27 @@
+// Add a language label per each code block
+$("pre").each(function (i, e) {
+    // Intialize with empty value
+    $(e).attr("data-title", " ");
+});
+
+$("pre code").each(function (i, e) {
+    var code = $(e);
+    var pre = code.parent();
+
+    var html_class = code.attr('class')
+    var language = html_class ? html_class.replace('language-', '') : "text";
+    var text_block = pre.text();
+
+    if (language == "python" && text_block.includes(">>> ")) {
+        language = "python repl";
+    }
+    else if (language == "bash") {
+        language = "shell";
+    }
+    
+    pre.attr("data-title", language);
+});
+
 // Scrollbar Width function
 function getScrollBarWidth() {
     var inner = document.createElement('p');
